a7cb39d3980c6cc27a9a01655c4931901bd44042
Vova <vova77gh@gmail.com>
2019-12-12 09:40:48 +0200

    part 4, start game, html and css

--- part-3__DOM/9. click/script.js
+++ part-3__DOM/9. click/script.js
@@ -1,9 +1,8 @@
 /* document.querySelector('.wrapper').addEventListener('click', function (event) {  // or for id => getElementById 
-    let tagName = event.target.tagName.toLowerCase()
+    let tagName = event.target.tagName.toLowerCase() // name of tag in lower case (div, li ...)
+    //event.target => where was click?
 
     if(tagName === 'p'){
         event.target.style.color = 'blue'
     }
 }) */
-
-document.getElementById('')

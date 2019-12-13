ec29bb02190d158421726d92d28286191477ce13
Vova <vova77gh@gmail.com>
2019-12-13 18:56:33 +0200

    generate random square

--- part-5__Game/2.start_game/index.js
+++ part-5__Game/2.start_game/index.js
@@ -1,9 +1,18 @@
 const $start = document.querySelector('#start')
 const $game = document.querySelector('#game')
 
+let score = 0
+
 $start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
 
 
+function handleBoxClick(event) {    // Listening clicks
+    if(event.target.dataset.box){
+        score++
+        renderBox()     //if click on square (dataset===box)  =>  render New square
+    }
+}
 
 function startGame() {
     $start.classList.add('hide')    // hide btn with CSS class 'hide'
@@ -13,5 +22,25 @@ function startGame() {
 }
 
 function renderBox() {
-    
+    $game.innerHTML = ''    // clear square
+    let box = document.createElement('div')     // create square
+    let boxSize = getRandom(30, 100)    // get square width and height >30 and <100
+    let gameSize = $game.getBoundingClientRect()
+    let maxTop = gameSize.height - boxSize
+    let maxLeft = gameSize.width - boxSize
+
+    //style for square
+    box.style.height = box.style.width = boxSize + 'px'
+    box.style.position = 'absolute'
+    box.style.top = getRandom(0, maxTop) + 'px'
+    box.style.left = getRandom(0, maxLeft) + 'px'
+    box.style.backgroundColor = '#000'
+    box.style.cursor = 'pointer' 
+    box.setAttribute('data-box', 'true')   // set special attribute for square
+
+    $game.insertAdjacentElement('afterbegin', box)   // put square in game block
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
 }

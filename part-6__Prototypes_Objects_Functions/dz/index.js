cbe528f90bf0edd8aa949a3fbdfce42f58ed4aac
Vova <vova77gh@gmail.com>
2019-12-23 00:17:51 +0200

    dz 2.0

--- part-6__Prototypes_Objects_Functions/dz/index.js
+++ part-6__Prototypes_Objects_Functions/dz/index.js
@@ -9,12 +9,20 @@ Array.prototype.powArray = function () {
 
     return arrayToPow.map(function(i) { 
         
-        return Math.pow(i, 2) 
+        if (typeof i === 'string') {
+            return i.repeat(2)
+        }
+        else{
+            return Math.pow(i, 2) 
+        }
+
     })
 }
 
 
 let firstArray = new Array([1, 2, 3])
+let secondArray = new Array([5, 'Hello', 6])
 
 console.log(firstArray.powArray())
+console.log(secondArray.powArray())
 
